626e8c228de52026df8eedb948c74a3837aefd6e
Roshni Bhatt <39613541+rhbhatt@users.noreply.github.com>
2025-09-16 01:25:28 -0400

    Updates from lecture 09152025

--- Finished Code/gravity/main.go
+++ Finished Code/gravity/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"gifhelper"
 )
 
 func main() {
@@ -19,5 +21,52 @@ func main() {
 	}
 
 	//let's take CLAs: initial universe file, numGens, time, canvas width, drawing frequency
+	inputFile := "data/"+os.Args[1]+".txt"
 
+	initialUniverse, err := ReadUniverse(inputFile)
+	Check(err)
+
+	outputFile := "output/"+os.Args[1]
+
+	numGens, err2 := strconv.Atoi(os.Args[2])
+	Check(err2)
+
+	time, err3 := strconv.ParseFloat(os.Args[3],64)
+	Check(err3)
+
+	canvasWidth, err4 := strconv.Atoi(os.Args[4])
+	Check(err4)
+
+	drawingFrequency, err5 := strconv.Atoi(os.Args[5])
+	Check(err5)
+
+	if drawingFrequency <= 0 {
+		panic("Error: nonpositive number as drawingFrequency")
+	}
+
+	fmt.Println("Command line arguments read")
+
+	fmt.Println("simulating gravity.")
+
+	timePoints := SimulateGravity(initialUniverse, numGens, time)
+
+	fmt.Println("Simulation run")
+
+	fmt.Println("Drawing universes")
+
+	images := AnimateSystem(timePoints, canvasWidth, drawingFrequency)
+
+	fmt.Println("Images drawn")
+
+	fmt.Println("Making GIF")
+
+	gifhelper.ImagesToGIF(images, outputFile)
+	
+	fmt.Println("GIF drawn!")
+}
+
+func Check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }

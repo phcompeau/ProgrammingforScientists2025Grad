d244a522129aa90c391b6275515582b0a3603685
Roshni Bhatt <39613541+rhbhatt@users.noreply.github.com>
2025-09-17 20:02:52 -0400

    Updates from lecture 09172025

--- Finished Code/gravity/drawing.go
+++ Finished Code/gravity/drawing.go
@@ -5,9 +5,52 @@ import (
 	"image"
 )
 
-
+// constants used to animate the Jupiter simulation
+const jupiterMoonMultiplier = 10.0
+const trailFrequency = 10
+const numberOfTrailFrames = 100
+const trailThicknessFactor = 0.2
 
 // let's place our drawing functions here.
+
+// Draw trails behind the bodies in our animation
+// Inputs: canvas, map of trails for each body, frame counter, canvas and universe width, bodies
+// Note that this uses a pointer to the canvas as input.
+func DrawTrails(c *canvas.Canvas, trails map[int][]OrderedPair, frameCounter int, uWidth, canvasWidth float64, bodies []Body) {
+	for bodyIndex, b := range bodies {
+		trail := trails[bodyIndex]
+		numTrails := len(trail)
+
+		lineWidth := (b.radius/uWidth)*float64(canvasWidth)*trailThicknessFactor
+		if b.name == "Io" || b.name == "Ganymede" || b.name == "Callisto" || b.name == "Europa" {
+			lineWidth *= jupiterMoonMultiplier
+		}
+		c.SetLineWidth(lineWidth)
+
+		for j := 0; j < numTrails-1; j++ {
+			alpha := 255.0*float64(j)/float64(numTrails)
+			red := uint8((1-alpha/255.0)*255.0 + (alpha/255.0)*float64(b.red))
+			green := uint8((1-alpha/255.0)*255.0 + (alpha/255.0)*float64(b.green))
+			blue := uint8((1-alpha/255.0)*255.0 + (alpha/255.0)*float64(b.blue))
+
+			c.SetStrokeColor(canvas.MakeColor(red,green,blue))
+
+			startX := (trail[j].x / uWidth) * canvasWidth
+			startY := (trail[j].y / uWidth) * canvasWidth
+			endX := (trail[j+1].x / uWidth) * canvasWidth
+			endY := (trail[j+1].y / uWidth) * canvasWidth
+
+			c.MoveTo(startX,startY)
+			c.LineTo(endX,endY)
+			c.Stroke()
+		}
+
+	}
+}
+
+// Animate the simulation frame by frame
+// Input: slice of universe timepoints, width of canvas, frequency of frames drawn from timepoints 
+// Output: slice of images
 func AnimateSystem(timePoints []Universe, canvasWidth, drawingFrequency int) []image.Image {
 	images := make([]image.Image,0)
 
@@ -34,6 +77,8 @@ func AnimateSystem(timePoints []Universe, canvasWidth, drawingFrequency int) []i
 
 // DrawToCanvas generates the image corresponding to a canvas after drawing a Universe
 // object's bodies on a square canvas that is canvasWidth pixels x canvasWidth pixels
+// Input: universe, width of canvas, map of position trails for each body, counter corresponding to simulation frames
+// Output: image
 func DrawToCanvas(u Universe, canvasWidth int, trails map[int][]OrderedPair, frameCounter int) image.Image {
 	c := canvas.CreateNewCanvas(canvasWidth, canvasWidth)
 
@@ -42,7 +87,7 @@ func DrawToCanvas(u Universe, canvasWidth int, trails map[int][]OrderedPair, fra
 	c.ClearRect(0, 0, canvasWidth, canvasWidth)
 
 	// Draw trails for all bodies
-	//DrawTrails(&c, trails, frameCounter, u.width, float64(canvasWidth), u.bodies)
+	DrawTrails(&c, trails, frameCounter, u.width, float64(canvasWidth), u.bodies)
 
 	// Draw the bodies themselves
 	for _, b := range u.bodies {
@@ -63,8 +108,3 @@ func DrawToCanvas(u Universe, canvasWidth int, trails map[int][]OrderedPair, fra
 }
 
 
-
-const jupiterMoonMultiplier = 10.0
-const trailFrequency = 10
-const numberOfTrailFrames = 100
-const trailThicknessFactor = 0.2

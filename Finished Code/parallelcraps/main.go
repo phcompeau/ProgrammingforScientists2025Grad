c6ba473a7b91a1861187a4c8eef8489414432a67
Roshni Bhatt <39613541+rhbhatt@users.noreply.github.com>
2025-11-04 08:10:47 -0500

    Create main.go
    adding files from class 10292025

---
+++ Finished Code/parallelcraps/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	numTrials := 10000
+	numProcs := runtime.NumCPU()
+
+	start := time.Now()
+	ComputeHouseEdge(numTrials)
+	elapsed := time.Since(start)
+	fmt.Printf("Running serially took %s",elapsed)
+	fmt.Println()
+
+	start2 := time.Now()
+	ComputeHouseEdgeMultiProc(numTrials,numProcs)
+	elapsed2 := time.Since(start2)
+	fmt.Printf("Running in parallel took %s",elapsed2)
+	fmt.Println()
+
+	fmt.Println(float64(elapsed2)/float64(elapsed))
+}
